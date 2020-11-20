53fe5a696d3ba8c7088b8fc200c4094f655a5616
iougou03 <haesol820@naver.com>
2020-11-21 01:57:31 +0900

    update crawling

---
+++ Crawling/webToonRank.py
@@ -0,0 +1,37 @@
+import requests
+import datetime
+import csv
+from bs4 import BeautifulSoup
+
+req = requests.get('https://comic.naver.com/webtoon/weekday.nhn')
+html = req.text
+
+soup = BeautifulSoup(html,'html.parser')
+
+datas = soup.select(
+    '.col .col_inner'
+)
+
+ranking = {}
+for data in datas:
+    date = data.h4['class'][0]
+    if date not in ranking:
+        ranking[date] = []
+    for toon in data.ul.findChildren("li",recursive=False):
+        ranking[date].append(toon.a.img['title'])
+
+topN = 5
+for k,v in ranking.items():
+    print("date :",k)
+    for i in range(1,topN+1):
+        print(i,"등",v[i-1])
+
+with open('rank_webToon.csv','w',newline='') as c:
+    cp = csv.writer(c)
+    for k, v in ranking.items():
+        n=0
+        for vv in v:
+           n+=1 
+           cp.writerow([k,n,vv])
+    
+    
--- python/week5HW.py
+++ python/week5HW.py
@@ -2,14 +2,15 @@ import csv
 
 data=[['컴퓨터 구조','컴퓨터 비전','클라우드 컴퓨팅','SW창업 프로젝트','컴퓨터망프로그래밍','데이터과학기초'],
      ['이시형','정순기','김은주','이호경','서지훈','박혜영'],
-     [3,3,3,3,3,3]]
+     [3,3,3,3,3,3],
+     ['COMP411008','ITEC424001','GLSO227001','GLSO217002','GLSO229001','ITEC419001']]
 
 with open('Classes.csv','w',newline='') as c:
     cp = csv.writer(c)
-    cp.writerow(['Name','Professor','Point'])
+    cp.writerow(['Name','Professor','Point','Code'])
     i=0
     for course in data[0]: 
-        in_data = [course,data[1][i],data[2][i]]
+        in_data = [course,data[1][i],data[2][i],data[3][i]]
         cp.writerow(in_data)
         i+=1
 #------------------------------------------------
